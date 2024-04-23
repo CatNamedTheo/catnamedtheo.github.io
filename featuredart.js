e9c5117f6ccf3abc3f6d9a37c1874b5dcec4c7b3
CatNamedTheo <155766298+CatNamedTheo@users.noreply.github.com>
2024-04-23 17:08:11 -0300

    Delete featuredart.js

--- featuredart.js
+++
@@ -1,7 +0,0 @@
-const featuredArtList = [
-    ["2.jpg", "https://x.com/The_ChosenWoon", "@The_ChosenWoon"],
-    ["3.png", "https://x.com/jollepoker", "@jollepoker"],
-    ["4.gif", "https://x.com/jollepoker", "@jollepoker"],
-].map(value => ({ value, sort: Math.random() }))
-.sort((a, b) => a.sort - b.sort)
-.map(({ value }) => value);
