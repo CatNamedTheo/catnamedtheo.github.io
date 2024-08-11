0764000e222c3c11dce09c9d2df8f4ac036acc2e
CatNamedTheo <155766298+CatNamedTheo@users.noreply.github.com>
2024-08-11 09:57:14 -0300

    Update featuredart.js
    change how the seeding works.
    
    It now filters by artist and only allows for a certain amount of their art to be shown per reload.

--- featuredart.js
+++ featuredart.js
@@ -1,4 +1,4 @@
-const featuredArtList = [
+const unsortedArtList = [
     ["1.png", "https://x.com/nepnepu_2/status/1754642894500028532", "@nepnepu_2"],
     ["2.jpg", "https://x.com/The_ChosenWoon/status/1766246555018043843", "@The_ChosenWoon"],
     ["3.png", "https://x.com/jollepoker/status/1729583219098481068", "@jollepoker"],
@@ -86,69 +86,66 @@ const featuredArtList = [
     ["85.jpg", "https://x.com/Belphegoer_/status/1815059147487867361", "@Belphegoer_"],
     ["86.jpg", "https://x.com/grungoboi/status/1624382789343748099", "@grungoboi"],
     ["87.jpg", "https://x.com/grungoboi/status/1663227188366262284", "@grungoboi"],
-	["88.jpg", "https://x.com/ZXSTRLABORATORY/status/1781204272895385664", "@AmoebaLe"],
-	["89.jpg", "https://x.com/ZXSTRLABORATORY/status/1781204272895385664", "@AmoebaLe"],
-	["90.jpg", "https://x.com/ZXSTRLABORATORY/status/1781204272895385664", "@AmoebaLe"],
-	["91.jpg", "https://x.com/CristoCrisp/status/1760524193526092038", "@CristoCrisp"],
-	["92.jpg", "https://x.com/CristoCrisp/status/1773213418041815098", "@CristoCrisp"],
-	["93.png", "https://x.com/upupdowndownetc/status/1778301913253171696", "@upupdowndownetc"],
-	["94.jpeg", "https://x.com/TheTravelerHD/status/1802217510856474776", "@TheTravelerHD"],
-	["95.jpg", "https://x.com/TheTravelerHD/status/1676724668982517761", "@TheTravelerHD"],
-	["96.jpg", "https://x.com/TheTravelerHD/status/1630243534027763714", "@TheTravelerHD"],
-	["97.jpg", "https://x.com/TheTravelerHD/status/1732140833271624084", "@TheTravelerHD"],
-	["98.jpg", "https://x.com/TheTravelerHD/status/1756752174007062828", "@TheTravelerHD"],
-	["99.jpg", "https://x.com/grungoboi/status/1624382789343748099", "@grungoboi"],
-	["100.jpg", "https://x.com/grungoboi/status/1663227188366262284", "@grungoboi"],
-	["101.gif", "https://x.com/grungoboi/status/1634220016223637506", "@grungoboi"],
-	["102.jpg", "https://x.com/Heavic_/status/1767085177417707582", "@Heavic_"],
-	["103.png", "https://x.com/Heavic_/status/1686299564452564992", "@Heavic_"],
-	["104.jpeg", "https://x.com/Heavic_/status/1800493537362072019", "@Heavic_"],
-	["105.jpg", "https://x.com/Heavic_/status/1795010990523703394", "@Heavic_"],
-	["106.jpg", "https://x.com/SpookieJukes/status/1783946951450361922", "@SpookieJukes"],
-	["107.jpeg", "https://x.com/SpookieJukes/status/1779599629182480812", "@SpookieJukes"],
-	["108.jpeg", "https://x.com/SpookieJukes/status/1800231719167246437", "@SpookieJukes"],
-	["109.jpg", "https://x.com/SpookieJukes/status/1812968547007799358", "@SpookieJukes"],
-	["110.png", "https://x.com/SpookieJukes/status/1817370874418204966", "@SpookieJukes"],
-	["111.png", "https://x.com/animated15265/status/1795063367746388359", "@animated15265"],
-	["112.png", "https://x.com/animated15265/status/1795188202904469595", "@animated15265"],
-	["113.png", "https://x.com/animated15265", "@animated15265"],
-	["114.png", "https://x.com/animated15265", "@animated15265"],
-	["115.png", "https://x.com/animated15265", "@animated15265"],
-	["116.png", "https://x.com/animated15265", "@animated15265"],
-	["117.jpg", "https://x.com/KMantidae/status/1781044817654403464", "@KMantidae"],
-	["118.jpg", "https://x.com/KMantidae/status/1786810841104265717", "@KMantidae"],
-	["119.jpg", "https://x.com/Komeji_nouzy/status/1651031217519616007", "@Komeji_nouzy"],
-	["120.jpg", "https://x.com/Komeji_nouzy/status/1655757885815521280", "@Komeji_nouzy"],
-	["121.jpg", "https://x.com/Komeji_nouzy/status/1660073969494499328", "@Komeji_nouzy"],
-	["122.png", "https://x.com/Laced_Ends/status/1664327822129741825", "@Laced_Ends"],
-	["123.jpg", "https://x.com/Laced_Ends/status/1676674674606985234", "@Laced_Ends"],
-	["124.jpg", "https://x.com/meranyf__/status/1687180630029692928", "@meranyf__"],
-	["125.jpg", "https://x.com/meranyf__/status/1717680989135085753", "@meranyf__"],
-	["126.png", "https://x.com/meranyf__", "@meranyf__"],
-	["127.jpg", "https://x.com/MarvinCraft02", "@MarvinCraft02"],
-	["128.jpg", "https://x.com/MarvinCraft02", "@MarvinCraft02"],
-	["129.jpg", "https://x.com/MarvinCraft02", "@MarvinCraft02"],
-	["130.png", "-", "@runespoon001"],
-	["131.png", "-", "@runespoon001"],
-	["132.png", "-", "@runespoon001"],
-	["133.png", "-", "@runespoon001"],
-	["134.png", "-", "@runespoon001"],
-	["135.png", "-", "@runespoon001"],
-	["136.png", "-", "@runespoon001"],
-	["137.png", "-", "@wenpart"],
-	["138.png", "-", "@wenpart"],
-	["139.png", "-", "stanleyjhusdon_02365"],
-	["140.png", "-", "stanleyjhusdon_02365"],
-	["141.png", "-", "stanleyjhusdon_02365"],
-	["142.jpg", "https://x.com/YagiPagi/status/1764347159388868881", "@YagiPagi"],
-	["143.jpg", "https://x.com/YagiPagi/status/1676707906413002755", "@YagiPagi"],
-	["144.jpeg", "https://x.com/tsukimochiart/status/1818408034101522508", "@tsukimochiart"],
-	["145.jpg", "https://x.com/ACatNamedTheo1/status/1773438636995805664/", "@ACatNamedTheo1"],
-	["146.jpg", "https://x.com/ACatNamedTheo1", "@ACatNamedTheo1"],
-
-].map(value => ({ value, sort: Math.random() }))
-.sort((a, b) => a.sort - b.sort)
-.map(({ value }) => value);
+    ["88.jpg", "https://x.com/ZXSTRLABORATORY/status/1781204272895385664", "@AmoebaLe"],
+    ["89.jpg", "https://x.com/ZXSTRLABORATORY/status/1781204272895385664", "@AmoebaLe"],
+    ["90.jpg", "https://x.com/ZXSTRLABORATORY/status/1781204272895385664", "@AmoebaLe"],
+    ["91.jpg", "https://x.com/CristoCrisp/status/1760524193526092038", "@CristoCrisp"],
+    ["92.jpg", "https://x.com/CristoCrisp/status/1773213418041815098", "@CristoCrisp"],
+    ["93.png", "https://x.com/upupdowndownetc/status/1778301913253171696", "@upupdowndownetc"],
+    ["94.jpeg", "https://x.com/TheTravelerHD/status/1802217510856474776", "@TheTravelerHD"],
+    ["95.jpg", "https://x.com/TheTravelerHD/status/1676724668982517761", "@TheTravelerHD"],
+    ["96.jpg", "https://x.com/TheTravelerHD/status/1630243534027763714", "@TheTravelerHD"],
+    ["97.jpg", "https://x.com/TheTravelerHD/status/1732140833271624084", "@TheTravelerHD"],
+    ["98.jpg", "https://x.com/TheTravelerHD/status/1756752174007062828", "@TheTravelerHD"],
+    ["99.jpg", "https://x.com/grungoboi/status/1624382789343748099", "@grungoboi"],
+    ["100.jpg", "https://x.com/grungoboi/status/1663227188366262284", "@grungoboi"],
+    ["101.gif", "https://x.com/grungoboi/status/1634220016223637506", "@grungoboi"],
+    ["102.jpg", "https://x.com/Heavic_/status/1767085177417707582", "@Heavic_"],
+    ["103.png", "https://x.com/Heavic_/status/1686299564452564992", "@Heavic_"],
+    ["104.jpeg", "https://x.com/Heavic_/status/1800493537362072019", "@Heavic_"],
+    ["105.jpg", "https://x.com/Heavic_/status/1795010990523703394", "@Heavic_"],
+    ["106.jpg", "https://x.com/SpookieJukes/status/1783946951450361922", "@SpookieJukes"],
+    ["107.jpeg", "https://x.com/SpookieJukes/status/1779599629182480812", "@SpookieJukes"],
+    ["108.jpeg", "https://x.com/SpookieJukes/status/1800231719167246437", "@SpookieJukes"],
+    ["109.jpg", "https://x.com/SpookieJukes/status/1812968547007799358", "@SpookieJukes"],
+    ["110.png", "https://x.com/SpookieJukes/status/1817370874418204966", "@SpookieJukes"],
+    ["111.png", "https://x.com/animated15265/status/1795063367746388359", "@animated15265"],
+    ["112.png", "https://x.com/animated15265/status/1795188202904469595", "@animated15265"],
+    ["113.png", "https://x.com/animated15265", "@animated15265"],
+    ["114.png", "https://x.com/animated15265", "@animated15265"],
+    ["115.png", "https://x.com/animated15265", "@animated15265"],
+    ["116.png", "https://x.com/animated15265", "@animated15265"],
+    ["117.jpg", "https://x.com/KMantidae/status/1781044817654403464", "@KMantidae"],
+    ["118.jpg", "https://x.com/KMantidae/status/1786810841104265717", "@KMantidae"],
+    ["119.jpg", "https://x.com/Komeji_nouzy/status/1651031217519616007", "@Komeji_nouzy"],
+    ["120.jpg", "https://x.com/Komeji_nouzy/status/1655757885815521280", "@Komeji_nouzy"],
+    ["121.jpg", "https://x.com/Komeji_nouzy/status/1660073969494499328", "@Komeji_nouzy"],
+    ["122.png", "https://x.com/Laced_Ends/status/1664327822129741825", "@Laced_Ends"],
+    ["123.jpg", "https://x.com/Laced_Ends/status/1676674674606985234", "@Laced_Ends"],
+    ["124.jpg", "https://x.com/meranyf__/status/1687180630029692928", "@meranyf__"],
+    ["125.jpg", "https://x.com/meranyf__/status/1717680989135085753", "@meranyf__"],
+    ["126.png", "https://x.com/meranyf__", "@meranyf__"],
+    ["127.jpg", "https://x.com/MarvinCraft02", "@MarvinCraft02"],
+    ["128.jpg", "https://x.com/MarvinCraft02", "@MarvinCraft02"],
+    ["129.jpg", "https://x.com/MarvinCraft02", "@MarvinCraft02"],
+    ["130.png", "-", "@runespoon001"],
+    ["131.png", "-", "@runespoon001"],
+    ["132.png", "-", "@runespoon001"],
+    ["133.png", "-", "@runespoon001"],
+    ["134.png", "-", "@runespoon001"],
+    ["135.png", "-", "@runespoon001"],
+    ["136.png", "-", "@runespoon001"],
+    ["137.png", "-", "@wenpart"],
+    ["138.png", "-", "@wenpart"],
+    ["139.png", "-", "stanleyjhusdon_02365"],
+    ["140.png", "-", "stanleyjhusdon_02365"],
+    ["141.png", "-", "stanleyjhusdon_02365"],
+    ["142.jpg", "https://x.com/YagiPagi/status/1764347159388868881", "@YagiPagi"],
+    ["143.jpg", "https://x.com/YagiPagi/status/1676707906413002755", "@YagiPagi"],
+    ["144.jpeg", "https://x.com/tsukimochiart/status/1818408034101522508", "@tsukimochiart"],
+    ["145.jpg", "https://x.com/ACatNamedTheo1/status/1773438636995805664/", "@ACatNamedTheo1"],
+    ["146.jpg", "https://x.com/ACatNamedTheo1", "@ACatNamedTheo1"],
+];
 
 const artists = {
     "@nepnepu_2": "https://x.com/nepnepu_2",
@@ -157,19 +154,74 @@ const artists = {
     "@kiirokumart": "https://x.com/kiirokumart",
     "@Belphegoer_": "https://x.com/Belphegoer_",
     "@grungoboi": "https://x.com/grungoboi",
-	"@AmoebaLe": "https://x.com/AmoebaLe",
-	"@CristoCrisp": "https://x.com/CristoCrisp",
-	"@upupdowndownetc": "https://x.com/upupdowndownetc",
-	"@TheTravelerHD": "https://x.com/TheTravelerHD",
-	"@Heavic_": "https://x.com/Heavic_",
-	"@SpookieJukes": "https://x.com/SpookieJukes",
-	"@animated15265": "https://x.com/animated15265",
-	"@KMantidae": "https://x.com/KMantidae/status",
-	"@Komeji_nouzy": "https://x.com/Komeji_nouzy",
-	"@Laced_Ends": "https://x.com/Laced_Ends",
-	"@meranyf__": "https://x.com/meranyf__",
-	"@MarvinCraft02": "https://x.com/MarvinCraft02",
-	"@YagiPagi": "https://x.com/YagiPagi",
-	"@tsukimochiart": "https://x.com/tsukimochiart",
-	"@ACatNamedTheo1": "https://x.com/ACatNamedTheo1",
+    "@AmoebaLe": "https://x.com/AmoebaLe",
+    "@CristoCrisp": "https://x.com/CristoCrisp",
+    "@upupdowndownetc": "https://x.com/upupdowndownetc",
+    "@TheTravelerHD": "https://x.com/TheTravelerHD",
+    "@Heavic_": "https://x.com/Heavic_",
+    "@SpookieJukes": "https://x.com/SpookieJukes",
+    "@animated15265": "https://x.com/animated15265",
+    "@KMantidae": "https://x.com/KMantidae/status",
+    "@Komeji_nouzy": "https://x.com/Komeji_nouzy",
+    "@Laced_Ends": "https://x.com/Laced_Ends",
+    "@meranyf__": "https://x.com/meranyf__",
+    "@MarvinCraft02": "https://x.com/MarvinCraft02",
+    "@YagiPagi": "https://x.com/YagiPagi",
+    "@tsukimochiart": "https://x.com/tsukimochiart",
+    "@ACatNamedTheo1": "https://x.com/ACatNamedTheo1",
 }
+
+// Organize art by artist
+const artSortedByArtist = unsortedArtList.reduce((acc, [image, imageLink, artist]) => {
+    if (!acc[artist]) {
+        acc[artist] = [];
+    }
+    const artistLink = artists[artist];
+    acc[artist].push({ image, imageLink, artist, artistLink });
+    return acc;
+}, {});
+
+// Create a list of all available art pieces
+const allAvailableArt = Object.values(artSortedByArtist).flat();
+
+// Track appearances of each artist
+const artistAppearanceCount = {};
+
+// Create the featured art list
+const featuredArtList = [];
+
+// Maximum appearances per artist
+const maxAppearances = 12;
+
+while (allAvailableArt.length > 0) {
+    // Filter artists who can still be selected
+    const eligibleArtists = Object.keys(artSortedByArtist).filter(artist => {
+        return artSortedByArtist[artist].length > 0 &&
+               (!artistAppearanceCount[artist] || artistAppearanceCount[artist] < maxAppearances);
+    });
+
+    if (eligibleArtists.length === 0) {
+        break; // Exit loop if no eligible artists are left
+    }
+
+    // Randomly pick an artist from the eligible list
+    const randomArtist = eligibleArtists[Math.floor(Math.random() * eligibleArtists.length)];
+
+    // Pick a random art piece from the selected artist
+    const artistArtList = artSortedByArtist[randomArtist];
+    const randomIndex = Math.floor(Math.random() * artistArtList.length);
+    const [selectedArt] = artistArtList.splice(randomIndex, 1);
+    
+    // Add the selected art piece to the featured list
+    featuredArtList.push(selectedArt);
+
+    // Increment the appearance count for the artist
+    artistAppearanceCount[randomArtist] = (artistAppearanceCount[randomArtist] || 0) + 1;
+
+    // Remove the artist from the artSortedByArtist if no more art is left
+    if (artSortedByArtist[randomArtist].length === 0) {
+        delete artSortedByArtist[randomArtist];
+    }
+}
+
+console.log(featuredArtList);
