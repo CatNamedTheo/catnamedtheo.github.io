c130614cd7c383ca37452f787792bf7aa65f53fc
Jollepoker <danielvedin@gmail.com>
2024-10-22 22:02:50 +0200

    Fix text sizes on mobile

--- assets/scripts/nepSweeper.js
+++ assets/scripts/nepSweeper.js
@@ -556,7 +556,7 @@ class NepSweeper {
             const nepSweeperStyle  = document.createElement('link');
             nepSweeperStyle.id   = 'nepSweeperStyle';
             nepSweeperStyle.rel  = 'stylesheet';
-            nepSweeperStyle.href = './assets/styling/nepSweeperStyle.css?v=2.34';
+            nepSweeperStyle.href = './assets/styling/nepSweeperStyle.css?v=2.35';
             document.head.appendChild(nepSweeperStyle);
         }
 
