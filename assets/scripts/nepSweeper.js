e04c374b232f2b7e42d5aba72739d4e7f1f0ee41
Jollepoker <danielvedin@gmail.com>
2024-11-10 17:03:14 +0100

    Fix timezone for this weeks schedule

--- assets/scripts/nepSweeper.js
+++ assets/scripts/nepSweeper.js
@@ -614,7 +614,7 @@ class NepSweeper {
             const nepSweeperStyle  = document.createElement('link');
             nepSweeperStyle.id   = 'nepSweeperStyle';
             nepSweeperStyle.rel  = 'stylesheet';
-            nepSweeperStyle.href = './assets/styling/nepSweeperStyle.css?v=2.42';
+            nepSweeperStyle.href = './assets/styling/nepSweeperStyle.css?v=2.43';
             document.head.appendChild(nepSweeperStyle);
         }
 
